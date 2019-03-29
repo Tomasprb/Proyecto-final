10a78eb75b196803cb4262ce91fd0100f219a741
TOMAS PROTT BRILL <43718467@ort>
2019-03-29 09:29:24 -0300

    commit para ivan

---
+++ modelado y prototipo/Assets/MovimientoCamara.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCamara : MonoBehaviour {
+    public Transform trans;
+    public Vector3 offset;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = trans.position + offset;
+    }
+}
